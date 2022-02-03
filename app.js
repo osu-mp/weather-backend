9ca0e3b899bc78c2d8d2af4f729ff182143254c6
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-02-03 15:00:05 -0800

    Added code to secure weather and hello with hardcoded token

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var express = require('express')
 
 var app = express()
 
+const url = require('url');             // used to parse query params from url
+
 app.listen(3002)
 console.log('Node.jf Express server is running on port 3002...')
+
+const magic_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJyZWZyZXNoLXRva2VuIjoiZHVtbXlfdmFsdWUxIiwiYWNjZXNzLXRva2VuIjoiZHVtbXlfdmFsdWUyIiwiaWF0IjoxNTE2MjM5MDIyfQ.guTzYRwmwEx2WQbhc-4zFV7dLZw1uWNhUvnzL3fEt3o";
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
@@ -40,18 +44,47 @@ app.get('/v1/hello/', get_hello)
 app.post('/v1/auth/', post_auth)
 
 
+function check_token(req){
+    // function to check if user's input token matches the hardcoded token above
+    // Return true for authorized users, false otherwise
+    const queryObject = url.parse(req.url, true).query;
+    //console.log(queryObject);
 
-function get_weather(request, response){
-    response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":45.77,"feels_like":43.84,"temp_min":39.9,"temp_max":47.91,"pressure":1026,"humidity":88},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":100},"dt":1642217300,"sys":{"type":2,"id":2012991,"country":"US","sunrise":1642175199,"sunset":1642208235},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+    if (queryObject.token == magic_token)    {
+        return true;
+    }
+
+    return false;
 }
 
-function get_hello(request, response){
+function get_weather(req, response){
+    if (check_token(req)){
+        response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":45.77,"feels_like":43.84,"temp_min":39.9,"temp_max":47.91,"pressure":1026,"humidity":88},"visibility":10000,"wind":{"speed":0,"deg":0},"clouds":{"all":100},"dt":1642217300,"sys":{"type":2,"id":2012991,"country":"US","sunrise":1642175199,"sunset":1642208235},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+    }
+    else{
+        // TODO : should this return a different status code, how?
+        response.json({"Error": "Unauthorized user, no weather for you!"})
+    }
+}
+
+function get_hello(req, response){
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+
     console.log('Hello world requested');
-    response.json({"greeting": "Hello world!"})
+
+    if (check_token(req)){
+        response.json({"greeting": "Hello world!"})
+    }
+    else{
+        // TODO : should this return a different status code, how?
+        response.json({"greeting": "Unauthorized user, no hello for you!"})
+    }
+
 }
 
 function post_auth(request, response){
     //const user = request.body.user;
     const user = request.user;
-    response.send({"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJyZWZyZXNoLXRva2VuIjoiZHVtbXlfdmFsdWUxIiwiYWNjZXNzLXRva2VuIjoiZHVtbXlfdmFsdWUyIiwiaWF0IjoxNTE2MjM5MDIyfQ.guTzYRwmwEx2WQbhc-4zFV7dLZw1uWNhUvnzL3fEt3o"})
+    response.send({"token": magic_token})
 }
